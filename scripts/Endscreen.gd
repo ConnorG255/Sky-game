f3c35749a3b697073f1ff82eb02081bb8617e21c
conno <25connorgriffin@gmail.com>
2024-01-08 11:51:07 -0800

    Highscore

--- scripts/Endscreen.gd
+++ scripts/Endscreen.gd
@@ -1,7 +1,19 @@
 extends Control
 
 @onready var score = $PanelContainer/MarginContainer/VBoxContainer/Score
+@onready var high_score = $PanelContainer/MarginContainer/VBoxContainer/HighScore
 
+const save = "user://savefile.save"
+func _ready():
+	if(gethscore() < Global.score):
+		setscore()
+	
+	
+	
+func setscore():
+	var file = FileAccess.open(save, FileAccess.WRITE_READ)
+	file.store_32(Global.score)
+	
 func _on_restart_pressed():
 	Global.end = false
 	Global.score = 0
@@ -15,8 +27,19 @@ func _on_restart_pressed():
 func _process(delta):
 	
 	score.text = "Score: " + str(Global.score)
+	high_score.text = "Highscore: " + str(gethscore())
+	
 
+
+func gethscore():
+	var file = FileAccess.open(save, FileAccess.READ)
+	var hscore = 0
+	if FileAccess.file_exists(save):
+		hscore = file.get_32()
+	return hscore
+	
 
 func _on_home_pressed():
+	
 	get_tree().change_scene_to_file("res://prefabs/StartScreen.tscn")
 	pass # Replace with function body.
